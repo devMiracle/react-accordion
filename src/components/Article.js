62e19d0b19115635d2aa33e172a96699189385d7
Miracle <51058450+devMiracle@users.noreply.github.com>
2020-11-11 19:19:41 +0200

    Done!

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,28 +1,31 @@
-import {Component} from 'react'
+import React, { useState } from "react";
 
-export default class Article extends Component {
-    state = {
-        isOpen: true
-    }
-    handleToggleClick = () => {
-        this.setState({isOpen: !this.state.isOpen})
-    }
-    render() {
-        const {article} = this.props
-        const content = this.state.isOpen && <section className='card-text'>{article.content}</section>
-        return (
-            <div className='card mx-auto' style={{width:'50%'}}>
-                <div className='card-header'>
-                    <h2>
-                        {article.title}
-                        <button onClick={this.handleToggleClick} className='btn btn-primary btn-lg float-right'>Toggle</button>
-                    </h2>
-                </div>
-                <div className='card-body'>
-                    <section className='card-subtitle text-muted'>{(new Date(article.date)).toDateString()}</section>
-                    {content}
-                </div>
-            </div>
-        )
-    }
+export default function Article(props) {
+  const [isOpen, SetIsOpen] = useState(true);
+
+  const { article } = props;
+  const content = isOpen && (
+    <section className="card-text">{article.content}</section>
+  );
+  return (
+    <div className="card mx-auto" style={{ width: "50%" }}>
+      <div className="card-header">
+        <h2>
+          {article.title}
+          <button
+            onClick={() => SetIsOpen(!isOpen)}
+            className="btn btn-primary btn-lg float-right"
+          >
+            Toggle
+          </button>
+        </h2>
+      </div>
+      <div className="card-body">
+        <section className="card-subtitle text-muted">
+          {new Date(article.date).toDateString()}
+        </section>
+        {content}
+      </div>
+    </div>
+  );
 }
